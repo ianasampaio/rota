5afcf13b16a31ceb223a2a6d504f1eec6b8592a1
ianasampaio <iana.sampaio06@gmail.com>
2024-07-15 10:55:42 -0300

    refac: remove users useCase

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import AuthRouter from "./Auth";
-import UsersRouter from "./User";
 import ProductRouter from "./Product";
 import ClientRouter from "./Client";
 import { authenticate } from "../middlewares/AuthMiddleware";
@@ -8,7 +7,6 @@ import { authenticate } from "../middlewares/AuthMiddleware";
 const router = Router();
 
 router.use(AuthRouter);
-router.use(authenticate, UsersRouter);
 router.use(authenticate, ProductRouter);
 router.use(authenticate, ClientRouter);
 
