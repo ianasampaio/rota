04465f4fe81c539fd24ab6ab793c3f515bf3345b
ianasampaio <iana.sampaio06@gmail.com>
2024-07-16 14:12:39 -0300

    feat: add products to the shipment

---
+++ src/useCases/shipments/addShipmentProduct.ts
@@ -0,0 +1,26 @@
+import { body } from "express-validator";
+import prisma from "../../../prisma/client";
+import { ShipmentProductStatus } from "@prisma/client";
+
+export async function addShipmentProduct(payload: any) {
+  const { product_id, name, value, quantity } = payload.body;
+  const { id } = payload.params;
+
+  const status = ShipmentProductStatus.AVAILABLE;
+
+  const data = Array.from({ length: Number(quantity) }).map(() => ({
+    shipmentId: id,
+    productId: product_id,
+    name: name,
+    value: value,
+    status: status,
+  }));
+
+  await prisma.shipmentProduct.createMany({
+    data,
+  });
+
+  return {
+    statusCode: 201,
+  };
+}
