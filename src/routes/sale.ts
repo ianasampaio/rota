e6b4f58d7a478f71dc6c867a7e69a7afa1d739c3
ianasampaio <iana.sampaio06@gmail.com>
2024-09-01 18:11:55 -0300

    feat: add get sales by client functionality

--- src/routes/sale.ts
+++ src/routes/sale.ts
@@ -6,12 +6,14 @@ import { validatePayment, validateSale } from "../utils/schemas/sales/saleSchema
 import { listSales } from "../useCases/sales/listSales";
 import { getSale } from "../useCases/sales/getSale";
 import { addPaymentToSale } from "../useCases/sales/addPaymentToSale";
+import { getSalesByClient } from "../useCases/sales/getSalesByClient";
 
 const router = Router();
 
 router.post("/sales", validateData(validateSale),expressAdapter(createSale));
 router.get("/sales", expressAdapter(listSales));
 router.get("/sales/:id", expressAdapter(getSale));
+router.get("/sales/client/:clientId", expressAdapter(getSalesByClient));
 router.post("/sales/:id/payment", validateData(validatePayment), expressAdapter(addPaymentToSale));
 
 export default router;
